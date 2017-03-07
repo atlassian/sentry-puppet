ef551949d212fb5aa7b8979cf3b6a7a964fe74a5
Denis Sorokin <dsorokin@atlssian.com>
2017-03-06 17:23:57 -0800

    BBCOPS-884 Added manifests and template so sentry.yaml file can be managed via puppet

---
+++ manifests/config.pp
@@ -0,0 +1,10 @@
+ class sentry::config {
+
+   file{ '/etc/puppetlabs/puppet/sentry.yaml':
+     ensure  => file,
+     mode    => '0644',
+     owner   => $::sentry::user,
+     group   => $::sentry::group,
+     content => template('sentry/sentry.yaml.erb')
+   }
+
